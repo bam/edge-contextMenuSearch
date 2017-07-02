c0f9070caf1951e1951d8e5e11d71ed9e3835304
bam <andreybourgasov@gmail.com>
2017-07-02 17:59:47 +0300

    Added: * Options page; * Default search engines; * Custom engine adding;

--- src/background.js
+++ src/background.js
@@ -1,11 +1,48 @@
-browser.contextMenus.create({
-    id: 'contextSearch',
-    title: 'Search: "%s"',
-    contexts: ['selection'],
-    onclick: function (event) {
-        var query = event.selectionText.trim().replace(/\s/gi, '+');
-        browser.tabs.create({
-            url: 'https://www.google.ru/search?q=' + query
+function setDefaultProviders() {
+    browser.storage.local.get(null, function (result) {
+        if (!result.providers) {
+            result.providers = {
+                // TODO refactor string constants; move list of default providers to external module?
+                google: {
+                    name: 'Google',
+                    url: 'https://www.google.com/search?q='
+                },
+                yandex: {
+                    name: 'Yandex',
+                    url: 'https://www.yandex.ru/search/?text='
+                },
+                bing: {
+                    name: 'Bing',
+                    url: 'http://www.bing.com/search?q='
+                }
+            }
+        }
+        if (!result.currentProvider) {
+            result.currentProvider = 'google'
+        }
+
+        browser.storage.local.set(result, init);
+    })
+}
+
+function init() {
+    browser.storage.local.get(null, function (result) {
+        const currentProvider = result.providers[result.currentProvider]
+
+        browser.contextMenus.create({
+            id: 'contextSearch',
+            title: `Search with ${currentProvider.name}: "%s"`,
+            contexts: ['selection'],
+            onclick: function (event) {
+                const query = event.selectionText.trim().replace(/\s/gi, '+');
+                browser.storage.local.get(null, function (result) {
+                    browser.tabs.create({
+                        url: `${result.providers[result.currentProvider].url}${query}`
+                    })
+                })
+            }
         })
-    }
-})
+    })
+}
+
+setDefaultProviders();
---
+++ src/options.js
@@ -0,0 +1,119 @@
+document.addEventListener('DOMContentLoaded', function () {
+    browser.storage.local.get(null, (result) => {
+        const providersListEl = document.getElementById('providerslist');
+
+        initSelect(result.providers, result.currentProvider);
+        loadCustomProvidersList(result.providers, providersListEl);
+
+        document.getElementById('searchprovider').addEventListener('change', changeProvider)
+        document.getElementById('addbutton').addEventListener('click', addProvider)
+        document.getElementById('resetbutton').addEventListener('click', resetCustomProviders)
+    })
+})
+
+function resetCustomProviders() {
+    browser.storage.local.get('providers', (result) => {
+        var providers = {};
+
+        for (var p in result.providers) {
+            if (result.providers.hasOwnProperty(p)) {
+                var element = result.providers[p];
+
+                if (!element.custom) {
+                    providers[p] = element;
+                }
+            }
+        }
+
+        // TODO current provider global(external) constant
+        browser.storage.local.set({ providers, currentProvider: 'google' }, () => {
+            document.getElementById('providerslist').innerText = '';
+            document.getElementById('searchprovider').innerText = '';
+            // TODO current provider global(external) constant
+            initSelect(providers, 'google');
+        })
+    })
+}
+
+function changeProvider() {
+    browser.storage.local.set({ currentProvider: this.value }, () => {
+        browser.storage.local.get('providers', (result) => {
+            const currentProvider = result.providers[this.value];
+
+            browser.contextMenus.update('contextSearch', {
+                title: `Search with ${currentProvider.name}: "%s"`,
+            })
+        })
+    })
+}
+
+function loadCustomProvidersList(list, container) {
+    // TODO better iterating over object?
+    for (var p in list) {
+        if (list.hasOwnProperty(p)) {
+            var provider = list[p];
+            if (provider.custom) {
+                container.appendChild(createProviderDiv(provider.name, provider.url));
+            }
+        }
+    }
+}
+
+function initSelect(providers, currentProvider) {
+    const select = document.getElementById('searchprovider');
+
+    // TODO better iterating over object?
+    for (let p in providers) {
+        if (list.hasOwnProperty(p)) {
+            select.appendChild(createOption(p, providers[p].name));
+        }
+    }
+    select.value = currentProvider;
+}
+
+function createProviderDiv(name, url) {
+    const el = document.createElement('div');
+
+    el.innerText = `${name}: "${url}"`;
+
+    return el;
+}
+
+function createOption(value, text) {
+    const el = document.createElement('option')
+
+    el.value = value;
+    el.innerText = text;
+
+    return el;
+}
+
+function addProvider() {
+    const providerNameField = this.form.providername;
+    const queryStringField = this.form.querystring;
+    const providerName = providerNameField.value;
+    const queryString = queryStringField.value;
+    const basicHash = queryString.toLowerCase().replace(/\W/g, '_');
+
+    if (providerName !== '' && queryString !== '') {
+        browser.storage.local.get('providers', (result) => {
+            const providers = Object.assign(result.providers, {
+                [basicHash]: {
+                    name: providerName,
+                    url: queryString,
+                    custom: true
+                }
+            });
+
+            browser.storage.local.set(
+                { providers },
+                () => {
+                    document.getElementById('providerslist').appendChild(createProviderDiv(providerName, queryString));
+                    document.getElementById('searchprovider').appendChild(createOption(basicHash, providerName));
+                    providerNameField.value = '';
+                    queryStringField.value = '';
+                }
+            )
+        })
+    }
+}
