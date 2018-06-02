9032c992cbf2708f0a675073faee9c3b0aaa9558
bam <andreybourgasov@gmail.com>
2018-06-02 19:41:29 +0300

    added eslint

--- src/background.js
+++ src/background.js
@@ -1,100 +1,100 @@
 function setDefaultProviders() {
-    browser.storage.local.get(null, function (result) {
-        if (!result.providers) {
-            result.providers = {
-                // TODO refactor string constants; move list of default providers to external module?
-                google: {
-                    name: 'Google',
-                    url: 'https://www.google.com/search?q='
-                },
-                yandex: {
-                    name: 'Yandex',
-                    url: 'https://www.yandex.ru/search/?text='
-                },
-                bing: {
-                    name: 'Bing',
-                    url: 'http://www.bing.com/search?q='
-                }
-            }
+  browser.storage.local.get(null, function (result) {
+    if (!result.providers) {
+      result.providers = {
+        // TODO refactor string constants; move list of default providers to external module?
+        google: {
+          name: 'Google',
+          url: 'https://www.google.com/search?q='
+        },
+        yandex: {
+          name: 'Yandex',
+          url: 'https://www.yandex.ru/search/?text='
+        },
+        bing: {
+          name: 'Bing',
+          url: 'http://www.bing.com/search?q='
         }
-        if (!result.currentProvider) {
-            result.currentProvider = 'google'
-        }
+      }
+    }
+    if (!result.currentProvider) {
+      result.currentProvider = 'google'
+    }
 
-        browser.storage.local.set(result, init);
-    })
+    browser.storage.local.set(result, init);
+  })
 }
 
 function createGotoMenu(scheme, url) {
-    browser.contextMenus.create({
-        id: 'contextGoto',
-        title: `Go to: "${scheme}${url}"`,
-        contexts: ['selection', 'link'],
-        onclick: function () {
-            browser.tabs.create({
-                url: `${scheme}${url}`
-            })
-        }
-    })
+  browser.contextMenus.create({
+    id: 'contextGoto',
+    title: `Go to: "${scheme}${url}"`,
+    contexts: ['selection', 'link'],
+    onclick: function () {
+      browser.tabs.create({
+        url: `${scheme}${url}`
+      })
+    }
+  })
 }
 
 function init() {
-    browser.storage.local.get(null, function (result) {
-        const currentProvider = result.providers[result.currentProvider]
+  browser.storage.local.get(null, function (result) {
+    const currentProvider = result.providers[result.currentProvider]
 
-        browser.contextMenus.create({ // TODO Refactor move function outside
-            id: 'contextSearch',
-            title: `Search with ${currentProvider.name}: "%s"`,
-            contexts: ['selection'],
-            onclick: function (event) {
-                const query = event.selectionText.trim().replace(/\s/gi, '+');
-                browser.storage.local.get(null, function (result) {
-                    browser.tabs.create({
-                        url: `${result.providers[result.currentProvider].url}${query}`
-                    })
-                })
-            }
+    browser.contextMenus.create({ // TODO Refactor move function outside
+      id: 'contextSearch',
+      title: `Search with ${currentProvider.name}: "%s"`,
+      contexts: ['selection'],
+      onclick: function (event) {
+        const query = event.selectionText.trim().replace(/\s/gi, '+');
+        browser.storage.local.get(null, function (result) {
+          browser.tabs.create({
+            url: `${result.providers[result.currentProvider].url}${query}`
+          })
         })
+      }
     })
+  })
 }
 
 if (!window.browser) window.browser = chrome; // Compatibility for Chrome
 
 browser.runtime.onMessage.addListener(function (msg) {
-    browser.contextMenus.remove('contextGoto')
-    browser.contextMenus.remove('contextSearch')
+  browser.contextMenus.remove('contextGoto')
+  browser.contextMenus.remove('contextSearch')
 
-    if (msg) {
-        browser.storage.local.get(null, function (result) {
-            const currentProvider = result.providers[result.currentProvider]
+  if (msg) {
+    browser.storage.local.get(null, function (result) {
+      const currentProvider = result.providers[result.currentProvider]
 
-            const urlWithSchemeRegexp = /^(?:[a-z]+:)(?:\/\/)?(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)?(?:(?:\/[^\/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
-            const urlWithHostnameRegexp = /^(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)(?:(?:\/[^\/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
-            const msgForTest = msg.trim().toLowerCase();
+      const urlWithSchemeRegexp = /^(?:[a-z]+:)(?:\/\/)?(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)?(?:(?:\/[^\/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
+      const urlWithHostnameRegexp = /^(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)(?:(?:\/[^\/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
+      const msgForTest = msg.trim().toLowerCase();
 
 
 
-            browser.contextMenus.create({ // TODO Refactor move function outsides
-                id: 'contextSearch',
-                title: `Search with ${currentProvider.name}: ${msg}`,
-                contexts: ['selection', 'link'],
-                onclick: function (event) {
-                    const query = event.selectionText.trim().replace(/\s/gi, '+');
-                    browser.storage.local.get(null, function (result) {
-                        browser.tabs.create({
-                            url: `${result.providers[result.currentProvider].url}${query}`
-                        })
-                    })
-                }
+      browser.contextMenus.create({ // TODO Refactor move function outsides
+        id: 'contextSearch',
+        title: `Search with ${currentProvider.name}: ${msg}`,
+        contexts: ['selection', 'link'],
+        onclick: function (event) {
+          const query = event.selectionText.trim().replace(/\s/gi, '+');
+          browser.storage.local.get(null, function (result) {
+            browser.tabs.create({
+              url: `${result.providers[result.currentProvider].url}${query}`
             })
+          })
+        }
+      })
 
-            if (urlWithSchemeRegexp.test(msgForTest)) {
-                createGotoMenu('', msgForTest);
-            } else if (urlWithHostnameRegexp.test(msgForTest)) {
-                createGotoMenu('https://', msgForTest) // TODO use default scheme from settings 
-            }
-        })
-    }
+      if (urlWithSchemeRegexp.test(msgForTest)) {
+        createGotoMenu('', msgForTest);
+      } else if (urlWithHostnameRegexp.test(msgForTest)) {
+        createGotoMenu('https://', msgForTest) // TODO use default scheme from settings 
+      }
+    })
+  }
 })
 
 setDefaultProviders();
--- src/content_script.js
+++ src/content_script.js
@@ -1,8 +1,8 @@
 if (!window.browser) window.browser = chrome; // Compatibility for Chrome
 
 document.addEventListener("selectionchange", function (event) {
-    var selected = document.getSelection();
-    var selectedText = (selected.type == 'Range') ? selected.toString() : '';
+  var selected = document.getSelection();
+  var selectedText = (selected.type == 'Range') ? selected.toString() : '';
 
-    browser.runtime.sendMessage(selectedText);
+  browser.runtime.sendMessage(selectedText);
 })
