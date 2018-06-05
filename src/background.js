34ca261cd9783d4c91ef64f61cde737a18750e74
bam <andreybourgasov@gmail.com>
2018-06-05 23:16:21 +0300

    Multiple click handlers fix #14

--- src/background.js
+++ src/background.js
@@ -37,6 +37,17 @@ function createGotoMenu(scheme, url) {
     }
   })
 }
+function updateGotoMenu(scheme, url) {
+  browser.contextMenus.update('contextGoto', {
+    title: `Go to: "${scheme}${url}"`,
+    contexts: ['selection', 'link'],
+    onclick: function () {
+      browser.tabs.create({
+        url: `${scheme}${url}`
+      })
+    }
+  })
+}
 
 function init() {
   browser.storage.local.get(null, function (result) {
@@ -61,25 +72,21 @@ function init() {
 if (!window.browser) window.browser = chrome; // Compatibility for Chrome
 
 browser.runtime.onMessage.addListener(function (msg) {
-  browser.contextMenus.remove('contextGoto')
-  browser.contextMenus.remove('contextSearch')
-
   if (msg) {
     browser.storage.local.get(null, function (result) {
-      const currentProvider = result.providers[result.currentProvider]
+      const currentProvider = result.providers[result.currentProvider];
+
+      const isGotoEnabled = result.gotoEnabled;
 
       const urlWithSchemeRegexp = /^(?:[a-z]+:)(?:\/\/)?(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)?(?:(?:\/[^\/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
       const urlWithHostnameRegexp = /^(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)(?:(?:\/[^\/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
       const msgForTest = msg.trim().toLowerCase();
 
-
-
-      browser.contextMenus.create({ // TODO Refactor move function outsides
-        id: 'contextSearch',
+      browser.contextMenus.update('contextSearch', { // TODO Refactor move function outsides
         title: `Search with ${currentProvider.name}: ${msg}`,
         contexts: ['selection', 'link'],
-        onclick: function (event) {
-          const query = event.selectionText.trim().replace(/\s/gi, '+');
+        onclick: (event) => {
+          const query = msg.trim().replace(/\s/gi, '+');
           browser.storage.local.get(null, function (result) {
             browser.tabs.create({
               url: `${result.providers[result.currentProvider].url}${query}`
@@ -89,10 +96,28 @@ browser.runtime.onMessage.addListener(function (msg) {
       })
 
       if (urlWithSchemeRegexp.test(msgForTest)) {
-        createGotoMenu('', msgForTest);
+        if (isGotoEnabled)
+          updateGotoMenu('', msgForTest)
+        else {
+          createGotoMenu('', msgForTest);
+          browser.storage.local.set({ gotoEnabled: true });
+        }
       } else if (urlWithHostnameRegexp.test(msgForTest)) {
-        createGotoMenu('https://', msgForTest) // TODO use default scheme from settings 
+        if (isGotoEnabled) {
+          // TODO use default scheme from settings
+          updateGotoMenu('https://', msgForTest)
+        } else {
+          createGotoMenu('https://', msgForTest);
+          browser.storage.local.set({ gotoEnabled: true });
+        }
+      } else {
+        browser.contextMenus.remove('contextGoto')
+        browser.storage.local.set({ gotoEnabled: false });
       }
+    })
+  } else {
+    browser.contextMenus.update('contextSearch', {
+      contexts: ['selection']
     })
   }
 })
