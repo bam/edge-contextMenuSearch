d49a52bc44cd87f94967a911a79ad7f69419fd74
bam <andreybourgasov@gmail.com>
2018-08-20 21:04:57 +0300

    New logic using global vars istead of async functions; Fixes #21; Fixes #24;

--- src/background.js
+++ src/background.js
@@ -1,20 +1,26 @@
-function init() {
-  browser.storage.local.get(null, (result) => {
-    const currentProvider = result.providers[result.currentProvider];
+function init(initSettings) {
+  const settings = initSettings || {};
 
-    browser.contextMenus.create({ // TODO Refactor move function outside
-      id: 'contextSearch',
-      title: `${browser.i18n.getMessage('searchWith')} ${currentProvider.name}: "%s"`,
-      contexts: ['selection'],
-      onclick(event) {
-        const query = event.selectionText.trim().replace(/\s/gi, '+');
-        browser.storage.local.get(null, (res) => {
-          browser.tabs.create({
-            url: `${res.providers[res.currentProvider].url}${query}`,
-          });
-        });
-      },
-    });
+  // TODO use constant for name
+  window.contextMenuSearchLocals = {
+    currentProvider: settings.providers[settings.currentProvider],
+    defaultProtocol: settings.defaultProtocol,
+    urlDetected: false,
+  };
+
+  const locals = window.contextMenuSearchLocals;
+
+  browser.contextMenus.create({ // TODO Refactor move function outside
+    id: 'contextSearch',
+    title: `${browser.i18n.getMessage('searchWith')} ${locals.currentProvider.name}: "%s"`,
+    contexts: ['selection'],
+    onclick(event) {
+      const query = event.selectionText.trim().replace(/\s/gi, '+');
+
+      browser.tabs.create({
+        url: `${locals.currentProvider.url}${query}`,
+      });
+    },
   });
 }
 
@@ -51,8 +57,10 @@ function setDefaultStoreValues() {
     }
 
     if (shouldUpdate) {
-      browser.storage.local.set(result, init);
-    } else init();
+      browser.storage.local.set(result, () => {
+        init(result);
+      });
+    } else init(result);
   });
 }
 
@@ -60,7 +68,7 @@ function createGotoMenu(scheme, url) {
   browser.contextMenus.create({
     id: 'contextGoto',
     title: `${browser.i18n.getMessage('goTo')}: "${scheme}${url}"`,
-    contexts: ['selection', 'link'],
+    contexts: ['selection', 'link', 'editable'],
     onclick() {
       browser.tabs.create({
         url: `${scheme}${url}`,
@@ -71,7 +79,7 @@ function createGotoMenu(scheme, url) {
 function updateGotoMenu(scheme, url) {
   browser.contextMenus.update('contextGoto', {
     title: `${browser.i18n.getMessage('goTo')}: "${scheme}${url}"`,
-    contexts: ['selection', 'link'],
+    contexts: ['selection', 'link', 'editable'],
     onclick() {
       browser.tabs.create({
         url: `${scheme}${url}`,
@@ -80,60 +88,91 @@ function updateGotoMenu(scheme, url) {
   });
 }
 
-if (!window.browser) window.browser = chrome; // Compatibility for Chrome
+function updateLocals(settings) {
+  const keys = Object.keys(settings);
+  const { length } = keys;
 
-browser.runtime.onMessage.addListener((msg) => {
-  if (msg) {
-    browser.storage.local.get(null, (result) => {
-      const {
-        defaultProtocol,
-        gotoEnabled: isGotoEnabled,
-        providers,
-      } = result;
-      const currentProvider = providers[result.currentProvider];
+  for (let i = 0; i < length; i += 1) {
+    window.contextMenuSearchLocals[keys[i]] = settings[keys[i]].newValue;
+  }
+}
 
+function onStorageChange(changes) {
+  // TODO think to remove eslint ignore comments
+  delete changes.providers;// eslint-disable-line
+
+  if (changes.currentProvider) {
+    browser.storage.local.get('providers', (res) => {
+      changes.currentProvider = { newValue: res.providers[changes.currentProvider.newValue] };// eslint-disable-line
+      updateLocals(changes);
+    });
+  } else {
+    updateLocals(changes);
+  }
+}
+
+function handleMessage(msg) {
+  const locals = window.contextMenuSearchLocals;
+
+  if (locals.lastMsg !== msg) {
+    locals.lastMsg = msg;
+    const {
+      defaultProtocol,
+      // TODO (!) add setting and new var urlDetected
+      urlDetected,
+      currentProvider,
+    } = locals;
+
+    if (msg) {
+      // TODO move url detection outside
       const urlWithSchemeRegexp = /^(?:[a-z]+:)(?:\/\/)?(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)?(?:(?:\/[^/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
       const urlWithHostnameRegexp = /^(?:(?:\S+(?::\S*)?@)?(?:(?:[a-z]+[a-z\d-]*(?:\.[a-z]+[a-z\d-]*)+)|(?:\d{1,3}(?:\.\d{1,3}){3}))(?::\d+)?)(?:(?:\/[^/\s#?]+)+\/?|\/)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/gi;
       const msgForTest = msg.trim().toLowerCase();
 
       browser.contextMenus.update('contextSearch', { // TODO Refactor move function outsides
         title: `${browser.i18n.getMessage('searchWith')} ${currentProvider.name}: ${msg}`,
-        contexts: ['selection', 'link'],
+        contexts: ['selection', 'link', 'editable'],
         onclick() {
-          const query = encodeURIComponent(msg.trim());
-          browser.storage.local.get(null, (res) => {
-            browser.tabs.create({
-              url: `${res.providers[res.currentProvider].url}${query}`,
-            });
+          const query = encodeURIComponent(msg);
+          browser.tabs.create({
+            url: `${currentProvider.url}${query}`,
           });
         },
       });
 
       if (urlWithSchemeRegexp.test(msgForTest)) {
-        if (isGotoEnabled) {
+        if (urlDetected) {
           updateGotoMenu('', msgForTest);
         } else {
           createGotoMenu('', msgForTest);
-          browser.storage.local.set({ gotoEnabled: true });
+          locals.urlDetected = true;
         }
       } else if (urlWithHostnameRegexp.test(msgForTest)) {
-        if (isGotoEnabled) {
-          // TODO use default scheme from settings
+        if (urlDetected) {
           updateGotoMenu(defaultProtocol, msgForTest);
         } else {
           createGotoMenu(defaultProtocol, msgForTest);
-          browser.storage.local.set({ gotoEnabled: true });
+          locals.urlDetected = true;
         }
-      } else {
+      } else if (locals.urlDetected) {
         browser.contextMenus.remove('contextGoto');
-        browser.storage.local.set({ gotoEnabled: false });
+        locals.urlDetected = false;
       }
-    });
-  } else {
-    browser.contextMenus.update('contextSearch', {
-      contexts: ['selection'],
-    });
+    } else {
+      browser.contextMenus.update('contextSearch', {
+        contexts: ['selection'],
+      });
+      if (urlDetected) {
+        browser.contextMenus.remove('contextGoto');
+        locals.urlDetected = false;
+      }
+    }
   }
-});
+}
+
+if (!window.browser) window.browser = chrome; // Compatibility for Chrome
+
+browser.runtime.onMessage.addListener(handleMessage);
+browser.storage.onChanged.addListener(onStorageChange);
 
 setDefaultStoreValues();
--- src/content_script.js
+++ src/content_script.js
@@ -1,8 +1,28 @@
 if (!window.browser) window.browser = chrome; // Compatibility for Chrome
 
-document.addEventListener('selectionchange', () => {
-  const selected = document.getSelection();
-  const selectedText = (selected.type === 'Range') ? selected.toString() : '';
+function sendSelection(event) {
+  const activeEl = document.activeElement;
+  let selectedText;
+
+  if (event.target === activeEl && typeof activeEl.value === 'string') {
+    selectedText = activeEl.value.substring(activeEl.selectionStart, activeEl.selectionEnd);
+  } else {
+    selectedText = window.getSelection().toString();
+  }
 
   browser.runtime.sendMessage(selectedText);
+}
+
+document.addEventListener('mousedown', (e) => {
+  if (e.button === 2 && e.detail === 1) {
+    sendSelection(e);
+  }
 });
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Apps' || e.key === 'ContextMenu') {
+    sendSelection(e);
+  }
+});
+
+document.addEventListener('selectionchange', sendSelection);
